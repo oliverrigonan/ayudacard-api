ea05d8a701ffbbf42b4e6a1066c4dece9b250959
NoahOliverRIgonan <oliverrigonan@gmail.com>
2019-08-13 12:18:32 +0800

    API Complete CRUD Citizen

--- ayudacard-api/ApiControllers/ApiMstCitizenController.cs
+++ ayudacard-api/ApiControllers/ApiMstCitizenController.cs
@@ -22,15 +22,82 @@ namespace ayudacard_api.ApiControllers
                                Id = d.Id,
                                Surname = d.Surname,
                                Firstname = d.Firstname,
-                               Middlename = d.Middlename ?? "",
-                               Extensionname = d.Extensionname ?? "",
+                               Middlename = d.Middlename,
+                               Extensionname = d.Extensionname,
                                DateOfBirth = d.DateOfBirth.ToShortDateString(),
                                PlaceOfBirth = d.PlaceOfBirth,
+                               SexId = d.SexId,
                                Sex = d.MstSex.Sex,
+                               CivilStatusId = d.CivilStatusId,
                                CivilStatus = d.MstCivilStatus.CivilStatus,
+                               Height = d.Height,
+                               Weight = d.Weight,
+                               BloodTypeId = d.BloodTypeId,
+                               BloodType = d.MstBloodType.BloodType,
+                               GSISNumber = d.GSISNumber,
+                               HDMFNumber = d.HDMFNumber,
+                               PhilHealthNumber = d.PhilHealthNumber,
+                               SSSNumber = d.SSSNumber,
+                               TINNumber = d.TINNumber,
+                               AgencyEmployeeNumber = d.AgencyEmployeeNumber,
+                               CitizenshipId = d.CitizenshipId,
                                Citizenship = d.MstCitizenship.Citizenship,
+                               TypeOfCitizenshipId = d.TypeOfCitizenshipId,
+                               TypeOfCitizenship = d.MstTypeOfCitizenship.TypeOfCitizenship,
+                               DualCitizenshipCountry = d.DualCitizenshipCountry,
+                               ResidentialNumber = d.ResidentialNumber,
+                               ResidentialStreet = d.ResidentialStreet,
+                               ResidentialVillage = d.ResidentialVillage,
+                               ResidentialBarangayId = d.ResidentialBarangayId,
+                               ResidentialBarangay = d.MstBarangay.Barangay,
+                               ResidentialCityId = d.ResidentialCityId,
+                               ResidentialCity = d.MstCity.City,
+                               ResidentialProvinceId = d.ResidentialProvinceId,
+                               ResidentialProvince = d.MstProvince.Province,
+                               ResidentialZipCode = d.ResidentialZipCode,
+                               PermanentNumber = d.PermanentNumber,
+                               PermanentStreet = d.PermanentStreet,
+                               PermanentVillage = d.PermanentVillage,
+                               PermanentBarangayId = d.PermanentBarangayId,
+                               PermanentBarangay = d.MstBarangay1.Barangay,
+                               PermanentCityId = d.PermanentCityId,
+                               PermanentCity = d.MstCity1.City,
+                               PermanentProvinceId = d.PermanentProvinceId,
+                               PermanentProvince = d.MstProvince1.Province,
+                               PermanentZipCode = d.PermanentZipCode,
+                               TelephoneNumber = d.TelephoneNumber,
+                               MobileNumber = d.MobileNumber,
+                               EmailAddress = d.EmailAddress,
+                               OccupationId = d.OccupationId,
+                               Occupation = d.MstOccupation.Occupation,
+                               EmployerName = d.EmployerName,
+                               EmployerAddress = d.EmployerAddress,
+                               EmployerTelephoneNumber = d.EmployerTelephoneNumber,
+                               SpouseSurname = d.SpouseSurname,
+                               SpouseFirstname = d.SpouseFirstname,
+                               SpouseMiddlename = d.SpouseMiddlename,
+                               SpouseExtensionname = d.SpouseExtensionname,
+                               SpouseOccupationId = d.SpouseOccupationId,
+                               SpouseOccupation = d.MstOccupation1.Occupation,
+                               SpouseEmployerName = d.SpouseEmployerName,
+                               SpouseEmployerAddress = d.SpouseEmployerAddress,
+                               FatherSurname = d.FatherSurname,
+                               FatherFirstname = d.FatherFirstname,
+                               FatherMiddlename = d.FatherMiddlename,
+                               FatherExtensionname = d.FatherExtensionname,
+                               MotherSurname = d.MotherSurname,
+                               MotherFirstname = d.MotherFirstname,
+                               MotherMiddlename = d.MotherMiddlename,
+                               MotherExtensionname = d.MotherExtensionname,
+                               PictureURL = d.PictureURL,
+                               CitizenStatusId = d.CitizenStatusId,
                                CitizenStatus = d.MstCitizensStatus.CitizenStatus,
-                               MobileNumber = d.MobileNumber
+                               CreatedByUserId = d.CreatedByUserId,
+                               CreatedByUser = d.MstUser.Fullname,
+                               CreatedDateTime = d.CreatedDateTime.ToShortDateString(),
+                               UpdatedByUserId = d.UpdatedByUserId,
+                               UpdatedByUser = d.MstUser1.Fullname,
+                               UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
                            };
 
             return citizens.ToList();
@@ -192,7 +259,7 @@ namespace ayudacard_api.ApiControllers
                     return Request.CreateResponse(HttpStatusCode.NotFound, "Sex not found!");
                 }
 
-                var civilStatus = from d in db.MstCivilStatus where d.Id == objCitizen.CitizenStatusId select d;
+                var civilStatus = from d in db.MstCivilStatus where d.Id == objCitizen.CivilStatusId select d;
                 if (!civilStatus.Any())
                 {
                     return Request.CreateResponse(HttpStatusCode.NotFound, "Civil status not found!");
@@ -315,7 +382,7 @@ namespace ayudacard_api.ApiControllers
                 db.MstCitizens.InsertOnSubmit(newCitizen);
                 db.SubmitChanges();
 
-                return Request.CreateResponse(HttpStatusCode.OK);
+                return Request.CreateResponse(HttpStatusCode.OK, "");
             }
             catch (Exception e)
             {
@@ -459,7 +526,7 @@ namespace ayudacard_api.ApiControllers
                     updateCitizen.UpdatedDateTime = DateTime.Now;
                     db.SubmitChanges();
 
-                    return Request.CreateResponse(HttpStatusCode.OK);
+                    return Request.CreateResponse(HttpStatusCode.OK, "");
                 }
                 else
                 {
